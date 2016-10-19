09446e4f74207f5b1ce42ca36f2f5b666f986bb3
kosuke-ka <kosuke-ka@vivo.naist.jp>
2016-10-19 11:01:49 +0900

    remove #6 and #7

--- Code2.java
+++ Code2.java
@@ -1,9 +1,7 @@
 public class Hajimete {
     public static void main(String[] args){
-	// ugly #6
 	System.out.println("Good morning, NAIST.");
 
-	// take care #7
 	System.out.println("Good morning, NAIST.");
 
 	// something's gone wrong #8
