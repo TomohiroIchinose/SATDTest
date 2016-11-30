a0a46b4a7715df36aa6ca2e8f2c8ca29f120a908
Tomohiro <admin@example.com>
2016-11-30 23:10:55 +0900

    add code4 2

--- Code4.java
+++ Code4.java
@@ -2,9 +2,9 @@ public class Hajimete {
     public static void main(String[] args){
 	// may cause problem #11X
 	System.out.println("Good morning, Ikoma.");
-	/*
+	/* fixme
 		fixme
-	*/
+	fixme */
 	System.out.println("http://fixme.com");
 	System.out.println("http://fixme.com");
     }
